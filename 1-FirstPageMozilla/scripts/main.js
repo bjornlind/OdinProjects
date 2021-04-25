32dd3f8bf30878889648678f77d9f7a594dbf16f
Björn Lind <bjorn_92@yahoo.se>
2021-04-25 11:12:10 +0200

    Add change user button

--- 1-FirstPageMozilla/scripts/main.js
+++ 1-FirstPageMozilla/scripts/main.js
@@ -11,3 +11,34 @@ myImage.onclick = function(){
         myImage.setAttribute('src', 'images/firefox-icon.png');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+if(!localStorage.getItem('name'))
+{
+    setUserName();
+}
+else
+{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+function setUserName(){
+    let myName = prompt('Please enter username');
+    if(!myName)
+    {
+        setUserName();
+    }
+    else
+    {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+
+myButton.onclick = function ()
+{
+    setUserName();
+}
